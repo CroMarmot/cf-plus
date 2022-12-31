578c293a4a4ca4ee0e8b6a953d669e3ccfb41783
cromarmot <yexiaorain@gmail.com>
2022-12-31 14:53:51 +0800

    eslint

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.less']
+  styleUrls: ['./main-page.component.less'],
 })
 export class MainPageComponent {
-
-
+  constructor() {
+    window['consoleCheck'] = environment.consoleCheck;
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  consoleCheck: 'prod',
   production: true
 };
